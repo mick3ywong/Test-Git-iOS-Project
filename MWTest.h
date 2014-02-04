2ec7c87feb77c5583d2409b030543b321a304570
Mike Siew Choong Wong <mick3ywong@Mikes-MacBook-Pro.local>
2014-02-04 22:08:33 +0800

    added a test class

---
+++ MWTest.h
@@ -0,0 +1,13 @@
+//
+//  MWTest.h
+//  Git Test
+//
+//  Created by Mike Siew Choong Wong on 2/4/14.
+//  Copyright (c) 2014 mickeywong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MWTest : NSObject
+
+@end
---
+++ MWTest.m
@@ -0,0 +1,13 @@
+//
+//  MWTest.m
+//  Git Test
+//
+//  Created by Mike Siew Choong Wong on 2/4/14.
+//  Copyright (c) 2014 mickeywong. All rights reserved.
+//
+
+#import "MWTest.h"
+
+@implementation MWTest
+
+@end
